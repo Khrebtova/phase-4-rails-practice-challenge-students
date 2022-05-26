e4dbfd54e12dfc22c64770d331067576328f6d19
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-05-26 10:46:28 -0700

    add CURD functuanality to student and instructor controllers

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,2 +1,45 @@
 class InstructorsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    
+def index
+        @instructors = Instructor.all
+        render json: @instructors
+    end
+
+    def show
+        @instructor = Instructor.find(params[:id])
+        render json: @instructor
+    end
+
+    def create
+        @instructor = Instructor.create!(instructor_params)
+        render json: @instructor
+    end
+
+    def update
+        instructor = Instructor.find(params[:id])
+        instructor.update!(instructor_params)
+        render json: instructor
+    end
+
+    def destroy
+        instructor = Instructor.find(params[:id])
+        instructor.destroy!
+        head :no_content
+    end
+
+
+    private
+
+    def render_not_found_response
+        render json: { errors: "Record not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def instructor_params
+        params.permit(:name)
+    end
 end
